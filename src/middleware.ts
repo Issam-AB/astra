81adb0367d8eb1eb46cd3322f4def2be5f7368c1
unknown <issam.aboulfadl05@gmail.com>
2025-01-21 16:54:31 +0100

    refactor middleware to simplify home route handling and improve readability

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,16 @@
 import { NextResponse } from "next/server";
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-
-const isHomeRoute = createRouteMatcher(["/"]);
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
 export default clerkMiddleware((auth, req) => {
     const { userId } = auth();
 
-    // if there is user and home route is accessed, redirect to dashboard or any other protected route
-    if (userId && isHomeRoute(req)) {
+    // If user is signed in and tries to access home page
+    if (userId && req.nextUrl.pathname === "/") {
         return NextResponse.rewrite(new URL("/", req.url));
     }
+
+    // For all other routes, continue normally
+    return NextResponse.next();
 });
 
 export const config = {
